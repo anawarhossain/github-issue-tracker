fe23b9b5d0909e04cf88b5042430b3122908865e
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-03-07 02:00:13 +0600

    dashboard page Button and Issues count section added

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,4 +1,10 @@
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+const btnAll = document.getElementById("btnAll");
+const btnOpen = document.getElementById("btnOpen");
+const btnClosed = document.getElementById("btnClosed");
+const totalIssues = document.getElementById("totalIssues");
 
-console.log(searchBtn, searchInput)
+
+
+console.log(searchBtn, searchInput , btnAll, btnOpen, btnClosed, totalIssues);
